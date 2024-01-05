990aaef25f05c78fb34d039f5355066114c08c1d
Franco Campaiola <francocampaiola@hotmail.com>
2024-01-04 21:03:21 -0300

    Add animations in menu

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import AboutCard from "../AboutCard";
 import Experience from "../Experience";
 
-const Content = () => {
+const Content = ({}) => {
   return (
     <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
       <AboutCard />
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,33 +1,45 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 
 const Menu = () => {
+  const [menuItems, setMenuItems] = useState([
+    { id: "about", active: true },
+    { id: "experience", active: false },
+    { id: "projects", active: false },
+  ]);
+
+  const handleItemClick = (itemId) => {
+    const updatedMenuItems = menuItems.map((item) => {
+      return {
+        ...item,
+        active: item.id === itemId,
+      };
+    });
+    setMenuItems(updatedMenuItems);
+  };
+
   return (
-    <nav className="nav hidden lg:block" aria-label="In-page jump links">
+    <nav
+      className="nav hidden lg:block"
+      aria-label="In-page jump links"
+    >
       <ul className="mt-16 w-max">
-        <li>
-          <a className="group flex items-center py-3" href="#about">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-              About
-            </span>
-          </a>
-        </li>
-        <li>
-          <a className="group flex items-center py-3" href="#experience">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-              Experience
-            </span>
-          </a>
-        </li>
-        <li>
-          <a className="group flex items-center py-3" href="#projects">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-              Projects
-            </span>
-          </a>
-        </li>
+        {menuItems.map((item) => (
+          <li key={item.id}>
+            <a
+              className={`group flex items-center py-3 ${
+                item.active ? "active" : ""
+              }`}
+              href={`#${item.id}`}
+              onClick={() => handleItemClick(item.id)}
+            >
+              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                {item.id.charAt(0).toUpperCase() + item.id.slice(1)}
+              </span>
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
