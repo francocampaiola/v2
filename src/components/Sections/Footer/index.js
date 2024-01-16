92784756ea68ec8ebbf12075241a7836a51ca747
Franco Campaiola <francocampaiola@hotmail.com>
2024-01-16 16:35:58 -0300

    fix name Brittany

--- src/components/Sections/Footer/index.js
+++ src/components/Sections/Footer/index.js
@@ -137,7 +137,7 @@ const Footer = () => {
             >
               Brittany Chiang
             </a>
-            portfolio.
+            {' '}portfolio.
           </p>
         </footer>
       )}
